94944d190d7ff79b4318ef985bb4e4fffb60709b
mikochild2 <mikochild2@gmail.com>
2020-05-21 07:45:57 -0400

    add ebook subclass

---
+++ Ebooks.java
@@ -0,0 +1,12 @@
+public class Ebooks extends LibraryItem {
+    private String series;
+    private String format;
+    private String artist;
+
+    public Ebooks(long id, String title, Author[] authors, String publisher, int pages, String genre, String subgenre, int totalCopies, int availCopies, String series, String format, String artist) {
+        super(id, title, authors, publisher, pages, genre, subgenre, totalCopies, availCopies);
+        this.series = series;
+        this.format = format;
+        this.artist = artist;
+    }
+}
