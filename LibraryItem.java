587dba2bca2e239c3c3f1a5b160203b83dcea337
mikochild2 <mikochild2@gmail.com>
2020-05-21 07:32:29 -0400

    add methods

--- LibraryItem.java
+++ LibraryItem.java
@@ -1,13 +1,14 @@
 public class LibraryItem{
     private long id;
     private String title;
-    private Author [] authors;
+    private Author [] authors; //I want to be able to add multiple authors to an item
     private String publisher;
-    private int pages;
+    private int pages; // more of my subclasses use pages than not
     private String genre;
     private String subgenre;
     private int totalCopies;
     private int availCopies;
+    //more variables in my outline that I will add later after I get this working
 
 
     public LibraryItem(long id, String title, Author authors[], String publisher, int pages, String genre, String subgenre, int totalCopies, int availCopies){
@@ -20,5 +21,77 @@ public class LibraryItem{
         this.subgenre = subgenre;
         this.totalCopies = totalCopies;
         this.availCopies = availCopies; 
+    }
+
+    public void itemAdded(){
+        System.out.println(String.format("Book added!\nID: %S\n Title: %s\n'Author(s): %s\n Publisher: %s\n Genre: %s\n Subgenre: %s\nPages: %s\nTotal Copies: %s\n Available Copies: %s", id, title, authors, publisher, genre, subgenre, pages, totalCopies, availCopies));
+    }
+    
+
+    //Getters
+    public long getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getPublisher(){
+        return publisher;
+    }
+
+    public int getPages(){
+        return pages;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public String getSubgenre(){
+        return subgenre;
+    }
+    
+    public int getTotalCopies(){
+        return totalCopies;
+    }
+
+    public int getAvailCopies(){
+        return availCopies;
+    }
+
+    //setters
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setPublisher(String publisher){
+        this.publisher = publisher;
+    }
+
+    public void setPages(int pages){
+        this.pages = pages;
+    }
+
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+
+    public void setSubgenre(String subgenre){
+        this.subgenre = subgenre;
+    }
+    
+    public void setTotalCopies(int totalCopies){
+        this.totalCopies = totalCopies;
+    }
+
+    public void setAvailCopies(int availCopies){
+        this.availCopies = availCopies;
     }
 }
